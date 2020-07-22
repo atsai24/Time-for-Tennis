8ac444fa9943a88de83747bd62b1d40fda2dff54
atsai24 <60451704+atsai24@users.noreply.github.com>
2020-07-22 12:19:20 -0700

    update notebook and script

--- src/tennis_script.py
+++ src/tennis_script.py
@@ -9,3 +9,9 @@ def one_dim_scatterplot(data, ax, jitter=0.2, **options):
     ax.scatter(data, jitter, **options)
     ax.yaxis.set_ticklabels([])
     ax.set_ylim([-1, 1])
+    
+def get_sample_means(sampler, n_samples, size):
+    means = []
+    for _ in range(size):
+        means.append(np.mean(sampler.rvs(n_samples)))
+    return means
